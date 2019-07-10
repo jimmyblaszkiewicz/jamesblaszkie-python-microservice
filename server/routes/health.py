51d739e8e12e76f3613226ef84330b7ee5d84a80
Jimmy Blaszkiewicz <James.Blaszkiewicz@ibm.com>
2019-07-10 11:15:07 -0400

    added /answer route

--- server/routes/health.py
+++ server/routes/health.py
@@ -7,3 +7,11 @@ def health():
     """health route"""
     state = {"status": "UP"}
     return jsonify(state)
+
+@app.route('/answer/<number>')
+def CheckAnswerToLifeTheUniverseAndEverything(number):
+    content = {
+        'statement': 'The Answer to Life the Universe and Everything is ' + number + '.',
+        'check': 'The statement is ' + str(number == '42') + '!'
+    }
+    return jsonify(content)
